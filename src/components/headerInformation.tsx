cdda09f5d3e1bec6a2dd2112c9efb2855295c5c5
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-06-05 21:32:57 -0300

    update files and fix issues in the project

--- src/components/headerInformation.tsx
+++ src/components/headerInformation.tsx
@@ -1,18 +1,18 @@
 
 type Props = {
     name: string,
-    cargo: string,
+    position: string,
     introduction: string
 }
 
-export default function HeaderInformation({name, cargo, introduction} : Props) {
+export default function HeaderInformation({name, position, introduction} : Props) {
       return (
-         <section className="p-2 w-75">
+         <header className="p-2 w-75">
             <div>
                 <h1 className="text-lg font-black mb-1 text-white">{name}</h1>
-                 <p className="text-lg font-bold my-1 text-white ">{cargo}</p>
+                 <p className="text-lg font-bold my-1 text-white ">{position}</p>
             </div>
             <p className=" text-white"  >{introduction}</p>
-         </section>
+         </header>
       )
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,22 +3,31 @@ import HeaderInformation from "../components/headerInformation";
 import ProfilePhoto from '../assets/profilePhoto.jpg';
 import 'animate.css';
 
+const profileData = {
+   name: 'Italo Marcelo',
+   position: 'Desenvolvedor Frontend',
+   introduction: 'Cria interfaces bonitas, rápidas e intuitivas.'
+}
+
 export default function ProfileCardLayout() {
+
+   const {name, position, introduction} = profileData
       return (
-         <section className="bg-gradient-to-r from-indigo-500 from-0% to-emerald-500 
+         <main className="bg-gradient-to-r from-indigo-500 from-0% to-emerald-500 
          to-170% p-5 w-[325px] relative left-[-1em] h-100 rounded-[16px]   border lg:w-90 
           hover:scale-105  cursor-pointer animate__animated animate__flipInY z-100 margin-fix" >
              <div className="box rounded-t-[16px] ">
-                </div>
+             </div>
               <div className="flex flex-col justify-center relative text-center items-center top-20 " >
                  <figure className="z-40 border-4 border-indigo-500  rounded-full overflow-hidden">
                     <img src={ProfilePhoto} alt="Italo Marcelo"  />
                  </figure>
                  <section className="z-40">
-                   <HeaderInformation name='Italo Marcelo' cargo='Desenvolvedor Frontend' introduction='Cria interfaces bonitas, rápidas e intuitivas.' />
+                   <HeaderInformation name={name} position={position}
+                    introduction={introduction} />
                 </section>
                 <BubbleSkills/>
               </div>
-         </section>
+         </main>
       )
 }
